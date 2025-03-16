23c2f9fa2afb40c47601af03512dbd3c241b8394
udivak <udivak9@gmail.com>
2025-03-16 17:03:11 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,4 @@
+def add_numbers(a, b):
+    return a + b
+if __name__ == "__main__":
+    print(f"3 + 5 = {add_numbers(3, 5)}")
---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import add_numbers
+class TestApp(unittest.TestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(0, 0), 0)
+if __name__ == "__main__":
+    unittest.main()
